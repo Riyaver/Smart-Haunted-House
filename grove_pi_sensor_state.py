722c91b516f2e6fbf0a93a174a576f09b0824ea5
Kirthika V <82315597+KirthikaV123@users.noreply.github.com>
2026-07-19 10:58:47 +0200

    Update grove_pi_sensor_state.py

--- grove_pi_sensor_state.py
+++ grove_pi_sensor_state.py
@@ -10,21 +10,20 @@ MQTT_PORT = 1883
 
 PIR_PORT = 2 #Digital
 ANGLE_SENSOR_PORT = 0 #Analog 
-ULTRASONIC_G2_PORT = 4 #Digital
+ULTRASONIC_G2_PORT = 4 #Digital 
 ULTRASONIC_G3_PORT = 7 #Digital
 LIGHT_SENSOR_PORT = 1 #Analog
-
+BUZZER_PORT = 8 
 
 NUMBER_OF_PEOPLE = 3
 ANGLE_THRESHOLD = 500
 ULTRASONIC_G2_DISTANCE_CM = 30
 ULTRASONIC_G3_DISTANCE_CM = 30
-LIGHT_THRESHOLD = 500
+LIGHT_THRESHOLD = 600
 
-
+#pir buzzer 
 
 sensor_state = {
-    "pir_triggered_3_people": False,
     "door_sensor_active": False,
     "angle_g1_sensor_active": False,
     "ultrasonic_g2_sensor_active": False,
@@ -32,18 +31,18 @@ sensor_state = {
     "light_g6_sensor_active": False
 }
 
-
 pir_people_count = 0
 previous_pir_value = 0
 
 
-client = mqtt.Client(callback_api_version=mqtt.CallbackAPIVersion.VERSION1)
+client = mqtt.Client()
 
 client.connect(MQTT_BROKER, MQTT_PORT, 60)
 client.loop_start()
 
 
 grovepi.pinMode(PIR_PORT, "INPUT")
+grovepi.pinMode(BUZZER_PORT, "OUTPUT")
 
 
 print("GrovePi sensor publisher started")
@@ -53,34 +52,27 @@ while True:
 
     try:
         # PIR SENSOR
-        if not sensor_state["pir_triggered_3_people"]:
+        if not sensor_state["door_sensor_active"]:
 
             pir_value = grovepi.digitalRead(PIR_PORT)
 
             if pir_value == 1 and previous_pir_value == 0:
                 pir_people_count += 1
-                print(
-                    f"People detected: "
-                    f"{pir_people_count}/{NUMBER_OF_PEOPLE}"
-                )
+                print("People detected:".format({pir_people_count}/{NUMBER_OF_PEOPLE}) )
 
             previous_pir_value = pir_value
 
             if pir_people_count >= NUMBER_OF_PEOPLE:
 
-                sensor_state["pir_triggered_3_people"] = True
+                print("Buzzer ON")
+                grovepi.digitalWrite(BUZZER_PORT, 1)
+
+                time.sleep(0.2)
+
                 sensor_state["door_sensor_active"] = True
 
                 client.publish(
-                    "house/state/pir_triggered_3_people",
-                    json.dumps({
-                        "state_name": "pir_triggered_3_people",
-                        "value": True
-                    })
-                )
-
-                client.publish(
-                    "house/state/door_sensor_active",
+                    "house/sensor/door_sensor_active",
                     json.dumps({
                         "state_name": "door_sensor_active",
                         "value": True
@@ -90,12 +82,14 @@ while True:
                 print("Three people detected")
                 print("Door closed")
                 print("Game sensors are now active")
-                print("Published: pir_triggered_3_people = True")
                 print("Published: door_sensor_active = True")
+
+                print("Buzzer OFF")
+                grovepi.digitalWrite(BUZZER_PORT, 0)
 
 
         #ALL SENSORS ACTIVE 
-        if sensor_state["pir_triggered_3_people"]:
+        if sensor_state["door_sensor_active"]:
 
             if not sensor_state["angle_g1_sensor_active"]:
 
@@ -106,7 +100,7 @@ while True:
                     sensor_state["angle_g1_sensor_active"] = True
 
                     client.publish(
-                        "house/state/angle_g1_sensor_active",
+                        "house/sensor/angle_g1_sensor_active",
                         json.dumps({
                             "state_name":
                                 "angle_g1_sensor_active",
@@ -115,29 +109,22 @@ while True:
                     )
 
                     print("Published: "
-                        "angle_g1_sensor_active = True"
-                    )
+                          "angle_g1_sensor_active = True")
 
 
-            # ULTRASONIC SENSOR — GAME 2
+            # ULTRASONIC SENSOR GAME 2
             if not sensor_state["ultrasonic_g2_sensor_active"]:
 
                 distance_g2 = grovepi.ultrasonicRead(
                     ULTRASONIC_G2_PORT
                 )
 
-                if (
-                    0 < distance_g2
-                    <= ULTRASONIC_G2_DISTANCE_CM
-                ):
+                if (0 < distance_g2 <= ULTRASONIC_G2_DISTANCE_CM):
 
-                    sensor_state[
-                        "ultrasonic_g2_sensor_active"
-                    ] = True
+                    sensor_state["ultrasonic_g2_sensor_active"] = True
 
                     client.publish(
-                        "house/state/"
-                        "ultrasonic_g2_sensor_active",
+                        "house/sensor/ultrasonic_g2_sensor_active",
                         json.dumps({
                             "state_name":
                                 "ultrasonic_g2_sensor_active",
@@ -151,27 +138,19 @@ while True:
                     )
 
 
-            # ULTRASONIC SENSOR — GAME 3
-            if not sensor_state[
-                "ultrasonic_g3_sensor_active"
-            ]:
+            # ULTRASONIC SENSOR GAME 3
+            if not sensor_state["ultrasonic_g3_sensor_active"]:
 
                 distance_g3 = grovepi.ultrasonicRead(
                     ULTRASONIC_G3_PORT
                 )
 
-                if (
-                    0 < distance_g3
-                    <= ULTRASONIC_G3_DISTANCE_CM
-                ):
+                if (0 < distance_g3 <= ULTRASONIC_G3_DISTANCE_CM):
 
-                    sensor_state[
-                        "ultrasonic_g3_sensor_active"
-                    ] = True
+                    sensor_state["ultrasonic_g3_sensor_active"] = True
 
                     client.publish(
-                        "house/state/"
-                        "ultrasonic_g3_sensor_active",
+                        "house/sensor/ultrasonic_g3_sensor_active",
                         json.dumps({
                             "state_name":
                                 "ultrasonic_g3_sensor_active",
@@ -186,13 +165,9 @@ while True:
 
 
             # LIGHT SENSOR
-            if not sensor_state[
-                "light_g6_sensor_active"
-            ]:
+            if not sensor_state["light_g6_sensor_active"]:
 
-                raw_light_value = grovepi.analogRead(
-                    LIGHT_SENSOR_PORT
-                )
+                raw_light_value = grovepi.analogRead(LIGHT_SENSOR_PORT)
 
                 if raw_light_value <= LIGHT_THRESHOLD:
 
@@ -201,7 +176,7 @@ while True:
                     ] = True
 
                     client.publish(
-                        "house/state/light_g6_sensor_active",
+                        "house/sensor/light_g6_sensor_active",
                         json.dumps({
                             "state_name":
                                 "light_g6_sensor_active",
@@ -216,9 +191,16 @@ while True:
 
 
     except IOError as error:
-        print(f"GrovePi error: {error}")
+        print("GrovePi error:".format({error}))
 
     except Exception as error:
-        print(f"Sensor error: {error}")
+        print("Sensor error: ".format({error}))
 
-    time.sleep(0.2)
+    except KeyboardInterrupt:
+        print("\nStopping sensor publisher")
+        grovepi.digitalWrite(BUZZER_PORT, 0)
+        client.loop_stop()
+        client.disconnect()
+        break
+
+time.sleep(0.5)
